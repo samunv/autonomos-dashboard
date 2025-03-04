44b97cca9d633e7764d9b1cae09dd0701ae7e3ed
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-03-04 16:41:58 +0100

    Arreglo texto

--- dashboard/src/lib/Filtros.svelte
+++ dashboard/src/lib/Filtros.svelte
@@ -59,7 +59,7 @@
       >
         <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
           <circle cx="50" cy="50" r="50" />
-        </svg>Facturas con más total
+        </svg>Facturas con mayor Total
       </li>
   </ul>
   </div>
